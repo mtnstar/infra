d5894ca0eab4f83de5f3bc5b5e8c7da24402b9c4
Mountain Star <github@mtnstar.net>
2026-03-24 07:10:48 +0000

    Try to fix wireguard server tests

--- molecule/wireguard/tests/test_wireguard-client-configs.py
+++ molecule/wireguard/tests/test_wireguard-client-configs.py
@@ -6,7 +6,7 @@ def test_wireguard_config_dir_exists_locally(molecule_inventory_dir):
     assert client_config_dir.exists(), f"{client_config_dir} missing on controller"
     assert client_config_dir.is_dir(), f"{client_config_dir} is not a directory on controller"
 
-def test_wireguard_client_config(host, molecule_inventory_dir):
+def test_wireguard_client_config(molecule_inventory_dir):
     client_config_dir: Path = molecule_inventory_dir / "wireguard_configs" / "clients" / "alice"
 
     assert client_config_dir.exists(), f"{client_config_dir} missing on controller"
